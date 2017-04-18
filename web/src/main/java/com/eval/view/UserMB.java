ae57aadd7f5ba5f86335f2b5100f07dd3c88df0d
Jorge Domínguez <jorge.dominguez@qacg.com>
2017-04-18 14:23:52 -0500

    cambios para mejora de vista

--- web/src/main/java/com/eval/view/UserMB.java
+++ web/src/main/java/com/eval/view/UserMB.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
